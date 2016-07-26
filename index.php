25605f4cf158e73b63d2ebb47c5a8d0951176331
Blair Englund <blair.englund@gmail.com>
2016-07-26 10:44:27 -0500

    populating the questions and options with JSON

--- index.php
+++ index.php
@@ -7,11 +7,11 @@
 <body>
 
 <form>
-<label><h2 id="question">what is blair's favorite dish?</h2></label>
-<input id="option1" type="radio" name="food" value="bibimbap">bibimbap<br>
-<input id="option2" type="radio" name="food" value="pierogi">pierogi<br>
-<input id="option3" type="radio" name="food" value="eggs benedict">eggs benedict<br>
-<input id="option4" type="radio" name="food" value="drunken noodles">drunken noodles<br>
+<label><h2 id="question"></h2></label>
+<input id="option1" type="radio" name="food" value="">&nbsp;<label id="option1label"></label><br>
+<input id="option2" type="radio" name="food" value="">&nbsp;<label id="option2label"></label><br>
+<input id="option3" type="radio" name="food" value="">&nbsp;<label id="option3label"></label><br>
+<input id="option4" type="radio" name="food" value="">&nbsp;<label id="option4label"></label><br>
 <input id="submit" type="button" name="submit" value="Submit">
 </form>
 
--- scripts.js
+++ scripts.js
@@ -1,13 +1,26 @@
 window.addEventListener('load', function() {
 
+	//Load the questions, options, and answers for the quiz
 	var loadq = new XMLHttpRequest()
 	loadq.open("get", "questions.txt");
 	loadq.send();
 
 	loadq.addEventListener('load', function(e){
+		//get the string in JSON and parse it out
 		var qstr = e.target.response;
 		var parsequestion = JSON.parse(qstr);
 		debugger;
+
+		questionfield.innerHTML = parsequestion.question;
+		option1.value = parsequestion.options.option1;
+		option2.value = parsequestion.options.option2;
+		option3.value = parsequestion.options.option3;
+		option4.value = parsequestion.options.option4;
+		option1label.innerHTML = parsequestion.options.option1;
+		option2label.innerHTML = parsequestion.options.option2;
+		option3label.innerHTML = parsequestion.options.option3;
+		option4label.innerHTML = parsequestion.options.option4;
+
 	})
 	
 	var button = document.getElementById('submit');
@@ -15,9 +28,13 @@ window.addEventListener('load', function() {
 	var questionfield = document.getElementById('question');
 
 	var option1 = document.getElementById('option1');
+	var option1label = document.getElementById('option1label');
 	var option2 = document.getElementById('option2');
+	var option2label = document.getElementById('option2label');
 	var option3 = document.getElementById('option3');
+	var option3label = document.getElementById('option3label');
 	var option4 = document.getElementById('option4');
+	var option4label = document.getElementById('option4label');
 	var display = document.getElementById('display');
 	var guess = '';
 
